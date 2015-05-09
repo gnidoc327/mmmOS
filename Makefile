af7b5a54302d500b389b07e7749a29ed35782b0b
kimhs327 <kimhs327@hanmail.net>
2015-05-10 03:27:43 +0900

    add queue and sync

--- Makefile
+++ Makefile
@@ -9,20 +9,27 @@ CFLAGS=-DX86 -m32 -msse2 -mfpmath=sse -O2
 
 #.c.s:
 #	$(CC) $(CFLAGS) $(CPPFLAGS) -S $<
-sthread.o: sthread.c
+
+sthread.o: sthread.c 
 	$(CC) $(CFLAGS) -c -o $@ sthread.c
 
-sync.o: sync.c
+sync.o: sync.c sync.h
 	$(CC) $(CFLAGS) -c -o $@ sync.c
 
 sample.o: sample.c
 	$(CC) $(CFLAGS) -c -o $@ sample.c
 
+st_queue.o: st_queue.c st_queue.h
+	$(CC) $(CFLAGS) -c -o $@ st_queue.c
+
 libsthread.a: sthread.o sync.o
 	$(AR) cr $@ $^
 
 sample: sample.o libsthread.a
-	$(CC) $(CFLAGS) -o $@ sample.o -L. -lsthread
+	$(CC) -o $@ sample.o -L. -lsthread
+
+test: test_sthreads.c sync.o sthread.o st_queue.o
+	$(CC) -o test test_sthreads.c sync.o sthread.o st_queue.o
 
 clean:
 	$(RM) libsthread.a sample *.o *~
---
+++ queue.c
@@ -0,0 +1,44 @@
+#include "st_queue.h"
+
+
+void stq_init(struct st_queue* queue){
+     queue->first = NULL;
+	 queue->last = NULL;
+}
+
+
+void stq_enqueue(struct st_queue* queue, sthread_t t){
+
+     struct st_node* node = malloc(sizeof(struct st_node));
+
+	 node->st = t;
+	 node->next = NULL;
+
+	 if (queue->last == NULL){
+	 	queue->last = node;
+		queue->first = node;
+	 } else {
+		queue->last->next = node;
+		queue->last = node;
+	 }
+}
+
+
+sthread_t stq_dequeue(struct st_queue* queue){
+
+     sthread_t rv = NULL;
+	 struct st_node* stn = NULL;
+
+	 if (queue->first = NULL){
+	 	return NULL;
+	 } else {
+		rv = queue->first->st;
+		stn = queue->first;
+		queue->first = queue->first->next;
+        if (queue->first == NULL){
+			queue->last = NULL;
+		}
+		free(stn);
+		return rv;
+	 }
+}
